d00834bba479b1740b7b9fe02c06b76e3d2ddd5d
Balázs Hangya <balazs.cshl@gmail.com>
2022-04-14 17:55:19 +0200

    update loaders

--- analysis/load/convert_IDs.m
+++ analysis/load/convert_IDs.m
@@ -1,12 +1,12 @@
 function IDs = convert_IDs(IDs)
 %CONVERT_IDS Converts IDs array to conventional IDs cell.
-%   IDS = CONVERT_IDS(IDS)  In IDS each row belongs to a cell. 
+%   IDS = CONVERT_IDS(IDS)  In IDS each row belongs to a cell.
 %   Output: #cells x 4 (animalId,recordingId, shankId, cellId) cell
 %   array containing strings.
 %   Possible input options:
 %   IDs: rowIds vector pointing to given rows of allCell table.
 %   IDs: partially numeric cell array (e.g. cellIds are numbers).
-%   
+%
 %   See also RASTER_PLOT, TWO_CYCLES_PHASEHISTOGRAM.
 
 %   Author: Barnabas Kocsis
@@ -15,11 +15,11 @@ function IDs = convert_IDs(IDs)
 
 if ~iscell(IDs) % if rowIds (in allCell matrix) are given
     global RESULTDIR
+    % Load data table
+    load(fullfile(RESULTDIR,'cell_features','allCell.mat'), 'allCell');
+    % Load map for allCell matrix (mO):
+    load(fullfile(RESULTDIR,'cell_features','allCellMap.mat'),'mO');
     for it = 1:numel(IDs)
-        % Load data table
-        load(fullfile(RESULTDIR,'cell_features','allCell.mat'), 'allCell');
-        % Load map for allCell matrix (mO):
-        load(fullfile(RESULTDIR,'cell_features','allCellMap.mat'),'mO');
         IDs2{it,1} = allCell(IDs(it), mO('animalId'));
         IDs2{it,2} = allCell(IDs(it), mO('recordingId'));
         IDs2{it,3} = allCell(IDs(it), mO('shankId'));
--- analysis/load/loadFieldPot.m
+++ analysis/load/loadFieldPot.m
@@ -1,4 +1,4 @@
-function fieldPot = loadFieldPot(animalIdN,recordingIdN)
+function fieldPot = loadFieldPot(animalIdN,recordingIdN,isPyr)
 %LOADFIELDPOT Loads hippo LFP data.
 %   FIELDPOT = LOADFIELDPOT(ANIMALIDN,RECORDINGIDN) loads fieldPot of a 
 %   recording, and cuts out stimulation (fmouse and opto) and 'noisy' parts
@@ -6,6 +6,7 @@ function fieldPot = loadFieldPot(animalIdN,recordingIdN)
 %   Parameters:
 %   ANIMALIDN: string (e.g. '20100304').
 %   RECORDINGIDN: string (e.g. '1').
+%   ISPYR: logical, flag, if exist and true: load pyramidal layer.
 %
 %   See also SAVE_HIPPOCAMPAL_FIELD, SAVE_SEPTAL_FIELD, 
 %   HIPPO_STATE_DETECTION, LOADTS.
@@ -21,7 +22,11 @@ global PREPROCDIR
 animalId = regexprep(animalIdN,'n',''); % remove n from filename begining
 recordingId = regexprep(recordingIdN,'n',''); % remove n from filename begining
 
-load(fullfile(PREPROCDIR,animalIdN,recordingIdN,[animalId,'_',recordingId,'_radiatum.mat']));
+if exist('isPyr','var') & isPyr % load pyramidal layer
+    load(fullfile(PREPROCDIR,animalIdN,recordingIdN,[animalId,'_',recordingId,'_pyramidal.mat']));
+else % load radiatum data
+    load(fullfile(PREPROCDIR,animalIdN,recordingIdN,[animalId,'_',recordingId,'_radiatum.mat']));
+end
 if ismember(PROJECTID,{'FREE_MOUSE','OPTOTAGGING'}) % cut out stimulation parts
     load(fullfile(ROOTDIR,'STIMULATIONS',[animalId,recordingId,'.mat']),'stim');
     fieldPot(stim==1) = [];
